bd75ea88c5d9ef3942be3aa55eef5b0a469ef847
Johnson Gao <kinotech808@gmail.com>
2026-06-17 23:33:56 +0800

    fix(vercel): explicitly set localePrefix always to make /en /zh valid URLs

--- src/i18n/routing.ts
+++ src/i18n/routing.ts
@@ -3,6 +3,8 @@ import { defineRouting } from "next-intl/routing";
 export const routing = defineRouting({
   locales: ["en", "zh"],
   defaultLocale: "en",
+  localePrefix: "always",
 });
 
 export type Locale = (typeof routing.locales)[number];
+
